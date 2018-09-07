611d4d88e307c6a5cb780542d42d71e59e1843e4
Mason <mwitcraft11@gmail.com>
2018-09-07 12:31:21 -0500

    Completely working, tested with writeData()
    Also cleaned up and added new comments.
    I think it is documented well enough.

--- read_range.c
+++ read_range.c
@@ -1,109 +1,19 @@
 #include <stdio.h>
-#include <string.h>
 
+/* Character representing the lowest character to count in this program */
 char low;
+/* Character representing the highest character to count in this program */
 char high;
+/* Int array of size 126 with each index being the int representation of an ASCII character */
 int data[126];
 
-int main(int argv, char* argc[]){
-
-	/* Simple ideas */
-	/* create an array of size 125 to store all data */
-	/* index will correspond to the character it represents (ascii decimal number) */
-	/* value at that index will be number of time the character appears */
-	/* or of size 125-32 and just add 32 to index when printing so correct character gets printed */
-	/* maybe add a high and low number corresponding to ascii # of high and low */
-
-	/* Process: */
-	/* 1. read file in */
-	/* 2. initialize data array */
-	/* 3. step through each line */
-	/* 4. step through each character in line */
-	/* 5. determine if character is in range */
-	/* 6. if it is, add to data array */
-	/* 7. print */
-	
-	/* char low; */
-	/* char high; */
-
-	/* int data[125]; */
-	for(int i = 0; i < 126; ++i)
-		data[i] = 0;
-
-/* 	Will include all characters from space to '}' (close curly bracket)	 */
-/*	space = 32 | '}' = 125 	 */
-	if(argv == 1){
-		low = ' ';
-		high = '}';
-		/* printf("Low = %c (%d)\n", low, low); */
-		/* printf("High = %c (%d)\n", high, high); */
-	}
-/*	Will range from first character to '}'  */
-	if(argv == 2){
-		low = argc[1][0];
-		high = '}';
-		/* printf("Low = %c (%d)\n", low, low); */
-		/* printf("High = %c (%d)\n", high, high); */
-	}
-/*	Will range from first character to second character  */
-	if(argv == 3){
-		low = argc[1][0];
-		high = argc[2][0];
-		/* printf("Low = %c (%d)\n", low, low); */
-		/* printf("High = %c (%d)\n", high, high); */
-	}
-
-	FILE *file = stdin;
-	char line[99999];
-
-	char c;
-
-	/* for(int i = 0; i < 125; ++i){ */
-	/* 	printf("Index: %i \t Count: %i\n", i, data[i]); */
-	/* } */
-        /*  */
-	/* printf("Index: 125 \t Count: %i\n", data[125]); */
-        /*  */
-
-	while(!feof(file)){
-		c = fgetc(file);
-		/* if(c == '}') */
-		/* 	printf("Close Bracket"); */
-		int charNum = (int)c;
-		if(c >= low && c <= high){
-			data[charNum] = ++data[charNum];
-		}
-	}
-	
-	printData();
-	writeData();
-	
-	/* c = fgetc(file); */
-	/* while(c != EOF){ */
-	/* 	printf("%c", c); */
-	/* 	c = fgetc(file); */
-	/* } */
-
-	/* while(fgets(line, sizeof(line), file)){ */
-	/* 	for(int i = 0; i < strlen(line); ++i){ */
-	/* 		char c = line[i]; */
-	/* 		int charNum = (int)c; */
-	/* 		if(c >= low && c <= high){ */
-	/* 			printf("ASCII value of %c = %d", c, c); */
-	/* 			printf("\t %i\n", charNum); */
-	/* 			data[charNum] = ++data[charNum]; */
-	/* 		} */
-	/* 	} */
-	/* } */
-
-	/* for(int i = (int)low; i <= (int)high; ++i){ */
-	/* 	#<{(| char indexChar = (char)i; |)}># */
-	/* 	printf("%c - %i\n", i, data[i]); */
-	/* } */
-
-	/* printData(); */
-}
-
+/* Prints out the contents of the data array */
+/* First line is shows the range that was provided in the format "Range: {a}-{b}" with 'a' being the low end of the range and 'b' being the high end of the range */
+/* Following lines, starting with the low end of range and ending with high end of range are in the format: */
+/* EX: */
+/* J:   5 ##### */
+/* Character being counted, followed by a colon, then number of times character appears in given file, right-aligned with 4 spaces initially between it and a colon,  */
+/* followed by x many hashes where x is how many times the character appears */
 void printData(){
 	printf("Range: %c-%c", low, high);
 	for(int i = (int)low; i <= (int)high; ++i){
@@ -117,6 +27,8 @@ void printData(){
 	printf("\n");
 }
 
+/* Exactly the same as printData(), but instead writes to a file */
+/* Used for testing with given examples */
 void writeData(){
 	FILE *outFile = fopen("DataOut", "w");
 	
@@ -128,4 +40,54 @@ void writeData(){
 		}
 	}
 	fprintf(outFile, "\n");
+}
+
+/* Reads command line arguments to determine range and reads in the provided file to be processed */
+/* Performs necessary action on the data array to allow it to be printed and written out */
+int main(int argv, char* argc[]){
+	
+	/* Initializes array with 0s at every index (because numbers represent number of characters in file) */
+	for(int i = 0; i < 126; ++i)
+		data[i] = 0;
+
+	/* Will include all characters from space to '}' (close curly bracket)	 */
+	/* space = 32 | '}' = 125 	 */
+	if(argv == 1){
+		low = ' ';
+		high = '}';
+	}
+
+	/* Will range from first character provided from command line argument to '}'  */
+	if(argv == 2){
+		low = argc[1][0];
+		high = '}';
+	}
+
+	/* Will range from first character to second character both supplied by command line arguments  */
+	if(argv == 3){
+		low = argc[1][0];
+		high = argc[2][0];
+	}
+
+	/* Assigns file provided to a variable */
+	FILE *file = stdin;
+	char c;
+
+	/* Steps through each character in the file provided and increments location in array corresponding  */
+	/* to the character (by index) 	 */
+	while(!feof(file)){
+		c = fgetc(file);
+		int charNum = (int)c; /* Gets int representation of char currently on */
+		/* Only increments data in array if the char is in the range specified */
+		if(c >= low && c <= high){
+			data[charNum] = ++data[charNum];
+		}
+	}
+
+	/* Prints data out to command line  */
+	printData();
+	/* Writes the data out to a file (used to test output with examples)  */
+	/* writeData(); */
+
+	return 0;
 }
